4b085ea753c6dcacd4a67cc62cf1b10ea5d81363
pruthvi011 <morepruthviraj05@gmail.com>
2025-09-26 10:40:47 +0530

    Started Learning Java with simple programs

---
+++ Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 30;
+        int sum  = a+b;
+        System.out.println("The sum is: " + sum);
+    }
+}
