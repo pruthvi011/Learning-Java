de3c73c034dc58bc97d41ff746e945489a90fdd9
pruthvi011 <morepruthviraj05@gmail.com>
2025-09-29 10:34:53 +0530

    Learned IF Else Syntax

---
+++ IfElseDemo.java
@@ -0,0 +1,12 @@
+public class IfElseDemo {
+    public static void main(String args[]){
+        int pass = 3214;
+
+        if(pass == 1234){
+            System.out.println("Welcome to Your Account!");
+        }
+        else{
+            System.out.println("Incorrect Password!");
+        }
+    }
+}
