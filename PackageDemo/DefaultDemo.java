56d4ebb239925672ceaf870d8a27bfd774fb478f
pruthvi011 <morepruthviraj05@gmail.com>
2025-10-31 10:55:00 +0530

    learning to access default methods from packages

---
+++ PackageDemo/DefaultDemo.java
@@ -0,0 +1,8 @@
+package package1;
+
+class DefaultDemo{
+	int no = 10;
+	void displayNumber(){
+		System.out.println("The Value: "+no);
+	}
+}
---
+++ PackageDemo/DefaultTest.java
@@ -0,0 +1,10 @@
+
+package package1;
+
+class DefaultTest {
+    public static void main(String[] args) {
+	DefaultDemo d1 = new DefaultDemo();
+	d1.displayNumber();
+        
+    }
+}
