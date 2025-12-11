2d10af2fae26c217d16068de123dfe577b374da7
pruthvi011 <morepruthviraj05@gmail.com>
2025-12-11 11:06:56 +0530

    tried to change the file permissions via set methods

--- FileHandling.java
+++ FileHandling.java
@@ -1,5 +1,33 @@
+import java.io.File;
+import java.io.IOException;
+
 public class FileHandling {
     public static void main(String[] args) {
-        
+        try {
+      File f1 = new File("C:\\JavaPrograms\\sample.txt"); // Create File object
+      if (f1.createNewFile()) {           // Try to create the file
+        System.out.println("File created: " + f1.getName());
+      } else {
+        System.out.println(f1.getName()+ "File already exists.");
+      }
+
+      System.out.println("File is Readable: "+f1.canRead());
+      System.out.println("File is Writable: "+f1.canWrite());
+      System.out.println("File is Executable: "+f1.canExecute());
+      
+      f1.setReadable(false);
+      f1.setWritable(false);
+      f1.setExecutable(true);
+
+      System.out.println("\n");
+
+      System.out.println("File is Readable: "+f1.canRead());
+      System.out.println("File is Writable: "+f1.canWrite());
+      System.out.println("File is Executable: "+f1.canExecute());
+
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace(); 
+    }
     }
 }
