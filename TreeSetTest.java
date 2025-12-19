ceeab33510f039e0a8b003b82030ace0727745d6
pruthvi011 <morepruthviraj05@gmail.com>
2025-12-19 08:56:07 +0530

    Using Pollfirst and PollLast Methods in TreeSet

---
+++ TreeSetTest.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class TreeSetTest {
+    public static void main(String[] args) {
+        TreeSet<Integer> t1 = new TreeSet<Integer>();
+        t1.add(27);
+        t1.add(86);
+        t1.add(21);
+        t1.add(34);
+        t1.add(78);
+        t1.add(31);
+        t1.add(19);
+
+        System.out.println("Tree Set: "+t1);
+
+        t1.pollFirst();
+        t1.pollLast();
+
+        System.out.println("Tree Set after Poll Methods: "+t1);
+
+
+    }
+}
