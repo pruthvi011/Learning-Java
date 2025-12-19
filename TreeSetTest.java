b7c341b95d86d8826bf3e8a5aa8338a17cd242e1
pruthvi011 <morepruthviraj05@gmail.com>
2025-12-19 09:34:49 +0530

    Understood Higher and Lower Methonds in Tree Set

--- TreeSetTest.java
+++ TreeSetTest.java
@@ -18,8 +18,18 @@ public class TreeSetTest {
         System.out.println("Tree Set after Poll Methods: "+t1);
 
 
-        System.out.println("Descending Tree Set: "+t1.descendingSet());// DescendinSet Method
+        System.out.println("Descending Tree Set: "+t1.descendingSet());// Descending Set Method
 
+        System.out.println("Head Tree Set: "+t1.headSet(34,true));//Head Set
+
+        System.out.println("Tail Tree Set: "+t1.tailSet(31,true));//Tail Set
+
+        System.out.println("SubSet Tree Set: "+t1.subSet(27,true,34,true));
+        //Subset
+
+        System.out.println("Highest of 19: "+t1.higher(19));//Highest of 19
+
+        System.out.println("Lowest of 19: "+t1.lower(27));//Lowest of 27
 
     }
 }
