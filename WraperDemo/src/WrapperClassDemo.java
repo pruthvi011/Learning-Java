8e5c20faef012c66ff5a4e71f780a3df94e4c0e3
pruthvi011 <morepruthviraj05@gmail.com>
2025-12-31 10:44:38 +0530

    Learned about Unboxing

--- WraperDemo/src/WrapperClassDemo.java
+++ WraperDemo/src/WrapperClassDemo.java
@@ -3,16 +3,26 @@ public class WrapperClassDemo {
         int i=20;
         int a=30;
 
-        Integer itr = Integer.valueOf(i);  //Autoboxing --- Explicite Conversion
 
+        //AUTOBOXING
+        Integer itr = Integer.valueOf(i);  //Autoboxing --- Explicite Conversion
         Integer j = a;  //Autoboxing --- Implicite Conversion
 
         System.out.println("i:"+i+" "+"a:"+a);
         System.out.println("itr:"+itr+" "+"j:"+j);
 
-        
+
         char ch = 'P';
         Character chr = Character.valueOf(ch);
         System.out.println("ch:"+ch+" chr:"+chr);
+
+        //UNBOXING
+        Integer num = new Integer(10); //created Wrapper Class Object
+        int n = num.intValue();  //Unboxing
+        System.out.println("num:"+num+" n:"+n);
+
+        Boolean redable = new Boolean(true); //created Wrapper Class Object
+        boolean read = redable.booleanValue(); //Unboxing
+        System.out.println("redable:"+redable+" read:"+read);
     }
 }
